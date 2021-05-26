0f31b81bc3c53354e103f2cbeeba198bb05dc34f
juliabenedicto <74271084+juliabenedicto@users.noreply.github.com>
2021-05-26 16:43:27 +0200

    Navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,37 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+    purge: ['./src/**/*.html', './src/**/*.js'],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        fontFamily: {
+            titles: ['Boogaloo'],
+            body: ['Lato']
+        },
+        colors: {
+            primary: '#388e3c',
+            transparent: 'transparent',
+            current: 'currentColor',
+            black: colors.black,
+            white: colors.white,
+            gray: colors.coolGray,
+            red: colors.red,
+            yellow: colors.amber,
+            green: colors.emerald,
+            blue: colors.blue,
+            indigo: colors.indigo,
+            purple: colors.violet,
+            pink: colors.pink,
+
+        },
+    },
+
+    extend: {
+
+    },
+
+    variants: {
+        extend: {},
+    },
+    plugins: [],
 }
